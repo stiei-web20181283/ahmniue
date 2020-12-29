4237b8fb09ce9f7dd6be7c0c8ac6990ee1232e15
Lexcubia <742681100@qq.com>
2020-12-29 15:09:02 +0800

    添加lombok

--- ahmniue-common/src/main/java/com/ahmniue/common/domain/SwaggerProperties.java
+++ ahmniue-common/src/main/java/com/ahmniue/common/domain/SwaggerProperties.java
@@ -11,36 +11,20 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = false)
 @Builder
 public class SwaggerProperties {
-    /**
-     * API文档生成基础路径
-     */
+    // API文档生成基础路径
     private String apiBasePackage;
-    /**
-     * 是否要启用登录认证
-     */
+    // 是否要启用登录认证
     private boolean enableSecurity;
-    /**
-     * 文档标题
-     */
+    // 文档标题
     private String title;
-    /**
-     * 文档描述
-     */
+    // 文档描述
     private String description;
-    /**
-     * 文档版本
-     */
+    // 文档版本
     private String version;
-    /**
-     * 文档联系人姓名
-     */
+    // 文档联系人姓名
     private String contactName;
-    /**
-     * 文档联系人网址
-     */
+    // 文档联系人网址
     private String contactUrl;
-    /**
-     * 文档联系人邮箱
-     */
+    // 文档联系人邮箱
     private String contactEmail;
 }
--- ahmniue-common/src/main/java/com/ahmniue/common/domain/WebLog.java
+++ ahmniue-common/src/main/java/com/ahmniue/common/domain/WebLog.java
@@ -10,59 +10,27 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class WebLog {
-    /**
-     * 操作描述
-     */
+    // 操作描述
     private String description;
-
-    /**
-     * 操作用户
-     */
+    // 操作用户
     private String username;
-
-    /**
-     * 操作时间
-     */
+    // 操作时间
     private Long startTime;
-
-    /**
-     * 消耗时间
-     */
+    // 消耗时间
     private Integer spendTime;
-
-    /**
-     * 根路径
-     */
+    // 根路径
     private String basePath;
-
-    /**
-     * URI
-     */
+    // URI
     private String uri;
-
-    /**
-     * URL
-     */
+    // URL
     private String url;
-
-    /**
-     * 请求类型
-     */
+    // 请求类型
     private String method;
-
-    /**
-     * IP地址
-     */
+    // IP地址
     private String ip;
-
-    /**
-     * 请求参数
-     */
+    // 请求参数
     private Object parameter;
-
-    /**
-     * 返回结果
-     */
+    // 返回结果
     private Object result;
 
 }
