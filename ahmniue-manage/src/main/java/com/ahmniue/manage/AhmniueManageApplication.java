138d74051bfcc1c6c7258d256ce25b52594f0e58
Lexcubia <742681100@qq.com>
2021-01-01 23:23:04 +0800

    修改项目启动映射指向根

--- ahmniue-manage/src/main/java/com/ahmniue/manage/AhmniueManageApplication.java
+++ ahmniue-manage/src/main/java/com/ahmniue/manage/AhmniueManageApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 应用启动入口
  */
 //@MapperScan("com.ahmniue.common.service")
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = "com.ahmniue")
+//@ComponentScan("com.ahmniue")
 public class AhmniueManageApplication {
     public static void main(String[] args) {
         SpringApplication.run(AhmniueManageApplication.class, args);
